0425b594731ff24919c3f7affb60cdd798bee5be
grey <Namefailedx@gmail.com>
2026-05-17 01:18:27 -0500

    Format pong-clone.js

--- pong-clone.js
+++ pong-clone.js
@@ -1,12 +1,12 @@
 // Pong Clone - classic paddle game
 
 let paddleHeight = 100;
-let paddleWidth = 15;
-let paddleSpeed = 10;
+let ballSpeedX, b
+llSpeedY;
 
-let leftY, rightY;
-let ballX, ballY;
-let ballSpeedX, ballSpeedY;
+
+
+
 let leftScore = 0;
 let rightScore = 0;
 let ballSize = 20;
@@ -20,14 +20,14 @@ function setup() {
 
 function draw() {
   background(20, 25, 30);
-  
+
   // Center line
   stroke(60, 70, 80, 50);
   strokeWeight(4);
   for (let i = 0; i < height; i += 30) {
     line(width / 2, i, width / 2, i + 15);
   }
-  
+
   // Draw paddles with glow
   noStroke();
   // Left paddle glow
@@ -36,24 +36,24 @@ function draw() {
   // Left paddle
   fill(100, 150, 255);
   rect(20, leftY, paddleWidth, paddleHeight, 4);
-  
+
   // Right paddle glow
   fill(255, 100, 100, 30);
   rect(width - 35, rightY - 5, paddleWidth + 10, paddleHeight + 10, 5);
   // Right paddle
   fill(255, 100, 100);
   rect(width - 30, rightY, paddleWidth, paddleHeight, 4);
-  
+
   // Draw ball with glow
   fill(255, 255, 255, 30);
   ellipse(ballX, ballY, ballSize * 1.5);
   fill(255, 255, 255);
   ellipse(ballX, ballY, ballSize);
-  
+
   // Move ball
   ballX += ballSpeedX;
   ballY += ballSpeedY;
-  
+
   // Ball collision with paddles
   if (ballX < 35 + paddleWidth && ballX > 20 && ballY > leftY && ballY < leftY + paddleHeight) {
     ballSpeedX *= -1.05;
@@ -65,12 +65,12 @@ function draw() {
     ballSpeedY += (ballY - (rightY + paddleHeight / 2)) * 0.1;
     ballX = width - 35 - paddleWidth - ballSize / 2;
   }
-  
+
   // Ball collision with walls
   if (ballY < ballSize / 2 || ballY > height - ballSize / 2) {
     ballSpeedY *= -1;
   }
-  
+
   // Score
   if (ballX < 0) {
     rightScore++;
@@ -80,21 +80,21 @@ function draw() {
     leftScore++;
     resetBall();
   }
-  
+
   // Limit ball speed
   ballSpeedX = constrain(ballSpeedX, -15, 15);
   ballSpeedY = constrain(ballSpeedY, -10, 10);
-  
+
   // Paddle movement
   if (keyIsDown(87)) leftY -= paddleSpeed; // W
   if (keyIsDown(83)) leftY += paddleSpeed; // S
   if (keyIsDown(UP_ARROW)) rightY -= paddleSpeed;
   if (keyIsDown(DOWN_ARROW)) rightY += paddleSpeed;
-  
+
   // Constrain paddles
   leftY = constrain(leftY, 0, height - paddleHeight);
   rightY = constrain(rightY, 0, height - paddleHeight);
-  
+
   // Score display
   fill(255, 255, 255, 80);
   noStroke();
@@ -102,7 +102,7 @@ function draw() {
   textAlign(CENTER, TOP);
   text(leftScore, width / 4, 40);
   text(rightScore, width * 3 / 4, 40);
-  
+
   // Instructions
   fill(255, 255, 255, 60);
   textSize(14);
